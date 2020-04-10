c01c238a8950a06303929f0e1aad6a73cd05c276
Wallace Augusto Bezerra <wabezerra@stefanini.com>
2020-04-10 16:00:31 -0300

    Código do capítulo 3

--- dotnet/csharp_fundamentals/src/GradeBook/Program.cs
+++ dotnet/csharp_fundamentals/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,24 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+    
+            List<double> grades = new List<double>();
+            grades.Add(12.7);
+            grades.Add(10.3);
+            grades.Add(6.11);
+            grades.Add(6.11);
+            grades.Add(56.1);
+
+            var result = 0.0;
+
+            foreach (var number in grades)
+            {
+                result += number;
+            }
+
+            result /= grades.Count;
+            System.Console.WriteLine($"The average grade is {result:N1}");
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
